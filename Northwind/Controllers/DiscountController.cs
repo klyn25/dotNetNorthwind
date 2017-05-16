92eca79ef96cd256e8bdc3367ae45d0a2cd1126b
Kristy Flick <kflick@my.wctc.edu>
2017-05-16 18:38:17 -0500

    Kristy - Discounts list w/add, edit, delete buttons

--- Northwind/Controllers/DiscountController.cs
+++ Northwind/Controllers/DiscountController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Northwind;
 
 namespace Northwind.Controllers
 {
     public class DiscountController : Controller
     {
         // GET: Discount
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            using (NorthwndEntities db = new NorthwndEntities())
+            {
+                //stuff goes here...
+                return View("Index", db.Discounts.OrderBy(p => p.EndTime).ToList()); ;
+
+                //var query = database.Posts.Join(database.Post_Metas,
+                //                post => database.Posts.Where(x => x.ID == id),
+                //                meta => database.Post_Metas.Where(x => x.Post_ID == id),
+                //                (post, meta) => new { Post = post, Meta = meta });
+            }
+
         }
 
         // GET: Discount/Add
